d96147b991de0968c860a9534624e68a7c8e0142
Filippa Hansen <filippahansen@Filippas-MacBook-Pro.local>
2023-09-26 09:57:15 +0200

    updates

--- project2/optimization/quasi_methods.py
+++ project2/optimization/quasi_methods.py
@@ -1,8 +1,8 @@
 import numpy as np
 from scipy import optimize
 
-# from methods import OptimizationMethod
-# from optimization_problem import OptimizationProblem
+from methods import OptimizationMethod
+from optimization_problem import OptimizationProblem
 import chebyquad_problem as cp
 import scipy.optimize as so
 from  scipy import dot,linspace
@@ -14,28 +14,29 @@ class QuasiNewton:
         self.G = G
         self.tol = tol
         self.maxIters = maxIters
-        self.gradient_function = gradient_function
 
     def good_broyden_minimize(self, x):
         """Combines the benefits of Broyden's Rank-1 update with
         Sherman-Morrison's formula for numerical stability and faster
         convergence."""
         iterations = 0
-        fx = self.gradient_function(x)
+        gradient = self.gradient_function(x)
         G = self.G
 
         # Check the frobenius norm and max iterations
-        while np.linalg.norm(fx) > self.tol and iterations < self.maxIters:
+        while np.linalg.norm(gradient) > self.tol and iterations < self.maxIters:
             # Solve for dx using the current Jacobian G
-            dx = np.linalg.solve(G, -fx)
+            G_reg = G + 1e-6 * np.eye(G.shape[0])
+            dx = np.linalg.solve(G_reg, -gradient)
             x = x + dx
-            fx_new = self.gradient_function(x)
-            dk = fx_new - fx
+            gradient_new = self.gradient_function(x)
+            print(gradient_new)
+            dr = gradient_new - gradient
 
             # Update the approximation of Jacobian matrix using Sherman-Morrison's formula
-            G = G + np.outer(dk - np.dot(G, dx), dx) / np.dot(dx, dx)
+            G = G + np.outer(dr - np.dot(G, dx), dx) / np.dot(dx, dx)
 
-            fx = fx_new
+            gradient = gradient_new
             iterations += 1
 
         print('Good broyden nbr iter: ', iterations)
@@ -46,7 +47,8 @@ class QuasiNewton:
         full Broyden update"""
         iterations = 0
         #  The Jacobian of the gradient is called Hessian and is symmetric.
-        H = np.linalg.inv(self.G)
+        G_reg = self.G + 1e-6 * np.eye(self.G.shape[0])
+        H = np.linalg.inv(G_reg)
         gradient = self.gradient_function(x)
 
         # Check the frobenius norm and max iterations
@@ -75,7 +77,8 @@ class QuasiNewton:
     def symmetric_broyden_minimize(self, x):
         """Symmetric Broyden update for Jacobian approximation."""
         iterations = 0
-        H = np.linalg.inv(self.G)
+        G_reg = self.G + 1e-6 * np.eye(self.G.shape[0])
+        H = np.linalg.inv(G_reg)
         gradient = self.gradient_function(x)
 
         while np.linalg.norm(gradient) > self.tol and iterations < self.maxIters:
@@ -104,21 +107,21 @@ def objective_function(x): # corr 2, 0, 0
 def main():
    
     x = np.array([0.0, 0.0])
-    # op = OptimizationProblem(fs, chebygrad)
-    # om = OptimizationMethod(op)
-    # G = om.hessian_aprox(x, 1) # - cannot reshape x of len 2....
+    op = OptimizationProblem(cp.chebyquad, cp.gradchebyquad)
+    om = OptimizationMethod(op, 0.01, 10)
+    G = om.hessian_aprox(x, 1) # - cannot reshape x of len 2....
     # print(G)
 
     # G needs to be initialized as a good approximation for the methods to work
     # therefore we need to use the hessian_aprox function here, until then this aprox
     # of the Hessian gives an ok approximation.
-    gradient_function = objective_function
+    #gradient_function = objective_function
 
    
-    G = np.array([[1, 2], [2, 16]])
+    #G = np.array([[1, 2], [2, 16]])
 
     # QN = QuasiNewton(cp.chebyquad_fcn,  maxIters=50)
-    QN = QuasiNewton(objective_function, gradient_function, G,  maxIters=100)
+    QN = QuasiNewton(cp.gradchebyquad, G,  maxIters=100)
 
     x_g = QN.good_broyden_minimize(x)
     x_b = QN.bad_broyden_minimize(x)
