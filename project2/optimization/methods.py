1f12ac7c82259c0fe00871905de82be71af0bd49
Filippa Hansen <filippahansen@Filippas-MacBook-Pro.local>
2023-09-28 19:55:59 +0200

    added rosenbrock function test on linesearch newton

--- project2/optimization/methods.py
+++ project2/optimization/methods.py
@@ -1,5 +1,8 @@
 import numpy as np
 import scipy.optimize as so
+from optimization_problem import OptimizationProblem
+import scipy.optimize as opt
+import matplotlib.pyplot as plt
 
 
 class OptimizationMethod():
@@ -42,8 +45,6 @@ class OptimizationMethod():
     def Newton_with_exact_line_search(self,x_init,step_size=0.00001,stopping_criteria=1e-20, maxIters=50):
         return self.Newton_help_method(x_init, 'exact_line_search',step_size,stopping_criteria,maxIters)
         
-        
-        return None
     
     
     def Newton_help_method(self,x_init,line_search,step_size=0.00001,stopping_criteria=1e-20, maxIters=50):
@@ -55,7 +56,7 @@ class OptimizationMethod():
         while (np.all(g>stopping_criteria) or iteration<maxIters):
             G = self.hessian_aprox(x, step_size)
             H = np.linalg.inv(G)
-            a = so.minimize_scalar(lambda a : self.opt_problem.function_value(x-a*np.dot(H,g))[0]).x if line_search == 'exact_line_search' else 1
+            a = so.minimize_scalar(lambda a : self.opt_problem.function_value(x-a*np.dot(H,g))).x if line_search == 'exact_line_search' else 1
             s = - a*np.dot(H,self.opt_problem.gradient_value(x))
             x = np.reshape(np.add(x,s),(len(x)))
             g = self.opt_problem.gradient_value(x)
@@ -64,47 +65,68 @@ class OptimizationMethod():
             
         return x
     
-        
-    def line_search_wolfe(self,xk, sk,c1=1e-20,c2=0.9, maxIter=50):
-        
-        """
-        xk                  : the x values at iteration k
-        sk                  : search direction = -H * gk at iteration k
-        c1                  : arbitrary constant between (0,1)
-        c2                  : arbitrary constant for the curvature condition between (c1,1)
-        
-        Implemented as per slide 21 in the course lecture
-        """
-        alfa_k_minus = 1 
-        
-        fx = np.array(self.opt_problem.function_value(xk))
-        dfx = np.array(self.opt_problem.gradient_value(xk))
-        
-        def phi(alfa):
-            return np.array(self.opt_problem.obj_function(xk + alfa*sk))
-        def dphi(alfa):
-            return np.array(self.opt_problem.gradient_value(xk + alfa*sk))
-        iteration = 0
-        # Armijo rule not fullfiled
-        while phi(alfa_k_minus) > fx + c1*alfa_k_minus* np.dot(dfx.T, sk) and iteration<maxIter:
-            alfa_k_minus = alfa_k_minus /2
-            iteration = iteration + 1
-        alfa_k_plus = alfa_k_minus 
-       # print(iteration)
-        iteration = 0
-        while phi(alfa_k_plus) <= fx + c1*alfa_k_plus* np.dot(dfx.T, sk) and iteration<maxIter:
-            alfa_k_plus = 2 * alfa_k_plus
-            iteration = iteration + 1
-       # print(iteration)
-        # Curvature rule not fullfiled 
-        iteration = 0
-        while np.dot(dphi(alfa_k_minus).T, sk) < c2 * np.dot(dfx.T, sk) and iteration<maxIter :
-            alfa_zero = (alfa_k_plus+alfa_k_minus)/2
-            if phi(alfa_zero) <= fx + c1*alfa_zero* np.dot(dfx.T, sk):
-                alfa_k_minus = alfa_zero
-            else:
-                alfa_k_plus = alfa_zero
-            iteration = iteration + 1
-        #print(iteration)
-        return alfa_k_minus        
+def rosenbrock_function(x):
+    return 100*(x[1] - (x[0])**2)**2 + (1-x[0])**2
+
+def plot_rosenbrock_function():
+    x = np.linspace(-0.5, 2, 500)
+    y = np.linspace(-1.5, 4, 500)
+    X, Y = np.meshgrid(x, y)
+    Z = rosenbrock_function([X, Y])
+
+    # Create the contour plot
+    contour_levels = 30
+    plt.figure(figsize=(10, 6))
+    contour = plt.contour(X, Y, Z ** 0.33, contour_levels, cmap='viridis')
+    plt.colorbar(contour, label='Rosenbrock function')
+
+    # Add labels and title
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Rosenbrock function')
+
+    plt.show()
+    plt.show()
+    
+
+def main():
+   
+    x = np.array([0.0, 1.0])
+    op = OptimizationProblem(rosenbrock_function)
+    om = OptimizationMethod(op)
+     # Store optimization path for plotting
+    optimization_path = [x.copy()]
+
+    while True:
+        x_new = om.Newton_with_exact_line_search(x)
+        optimization_path.append(x_new)
+        if np.linalg.norm(x_new - x) < 1e-6:
+            break
+        x = x_new
+
+    optimization_path = np.array(optimization_path)
+
+    print("Our Solution:", x)
+    print("Our Minimum Value:", rosenbrock_function(x))
+
+    # Use a numerical optimization algorithm to find the minimum
+    result = opt.minimize(rosenbrock_function, x, method='BFGS')
+
+    # The solution will be in result.x
+    solution = result.x
+    minimum_value = result.fun
+
+    print("Scipy Solution: (BFGS)", solution)
+    print("Scipy Minimum Value: (BFGS)", minimum_value)
+
+    plot_rosenbrock_function()
+    plt.close()
+
+
+
+if __name__ == "__main__":
+    main()
+
+    
+    
     
