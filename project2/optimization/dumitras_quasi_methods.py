d412af8e29367cc8d8124fa44d84bf1e4c006a7c
Jacob Forsell <jacobzeforsell@gmail.com>
2023-10-02 18:25:49 +0200

    add dumitra changes

--- project2/optimization/dumitras_quasi_methods.py
+++ project2/optimization/dumitras_quasi_methods.py
@@ -15,20 +15,20 @@ the Hessian of the problem. Study the quality of the approximation with growing 
 """
 import numpy as np
 from methods import OptimizationMethod
-from scipy.optimize import line_search
-
 
 class QuasiNewtonMethod(OptimizationMethod):
     
     def __init__(self, opt_problem):
     
-
         self.opt_problem = opt_problem 
 
     def minimize_bfgs(self, x_k, tol= 1e-10, maxIters = 50, save_H = False  ):
+        
         """
-        The minimization iteration. Not sure if there should be some extra conditions for positive def
-        for DFP(since BFGS preserves the pos def) or some other stopping condition?
+        x_k : initial values
+        tol : the maximum tolerated value for the gradient
+        maxIters : maximum number of iterations
+        save_H   : saves the computed hessians if needed
         
         """
       
@@ -37,18 +37,13 @@ class QuasiNewtonMethod(OptimizationMethod):
         reg = 0.0001 * np.eye(n, dtype= float)
         g_k = self.opt_problem.gradient_value(x_k)
         s_k = np.dot(-H_k,g_k)
-        hessians = []
-
+        hessians = [H_k]
         i = 0
-        # check the value of the gradient
         while i < maxIters and  np.linalg.norm(self.opt_problem.gradient_value(x_k)) > tol  :
-           # line search to find alfa_k
             try:
                np.linalg.cholesky(H_k)
             except:
                return False
-            
-           #### line search method ########
             alfa_k = self.line_search_wolfe(x_k, s_k)
 
             delta_k =alfa_k * s_k
@@ -57,15 +52,11 @@ class QuasiNewtonMethod(OptimizationMethod):
             g_updated = np.array(self.opt_problem.gradient_value(x_k))
             gamma_k =  g_updated - g_k
 
-           # bfgs update
-            # scalar
             gTHg =np.dot(np.dot(np.transpose(gamma_k),H_k), gamma_k)
             dTg =  np.dot(np.transpose(delta_k),gamma_k)
-            # nxn matrix 
             ddT =  np.dot(np.reshape(delta_k,(n,1)),np.reshape(delta_k, (1,n)))
             dgTH = np.dot(np.dot( np.reshape(delta_k, (n,1)), np.reshape(gamma_k, (1,n))), H_k)
             HgdT = np.dot(np.dot(H_k ,np.reshape(gamma_k, (n,1))) , np.reshape(delta_k, (1,n)))
-
 
 
             H_k = H_k + reg + ( 1 + gTHg / dTg) * (ddT / dTg) - (dgTH + HgdT) / dTg
@@ -73,53 +64,42 @@ class QuasiNewtonMethod(OptimizationMethod):
 
             i= i+1
             if save_H == True:
-                hessians = hessians.append(H_k)
+                hessians.append(H_k)
                 
         print("Number of iterations BFGS:", i-1)
-        print("x_k", x_k)
         return x_k, hessians
     
     
     def minimize_DFP (self, x_k, tol= 1e-10, maxIters = 50 ):
-        H_k = np.eye(len(x_k), dtype= float)
+       
+        n = len(x_k)
+        H_k = np.eye(n, dtype= float)
+        reg = 0.0001 * np.eye(n, dtype= float)
         g_k = self.opt_problem.gradient_value(x_k)
         s_k = np.dot(-H_k,g_k)
-        
         i = 0
-        # check the value of the gradient
+        
         while i < maxIters and  np.linalg.norm(self.opt_problem.gradient_value(x_k)) > tol  :
-            # line search to find alfa_k
             try:
               np.linalg.cholesky(H_k)
             except:
-            #  print("Number of iterations DFP:", i-1)
               return False
            
-           #### line search method ########
             alfa_k = self.line_search_wolfe(x_k, s_k)
-
-          # alfa_k=0.5
-           # calculating delta should be a nx1 vector
             delta_k =alfa_k * s_k
-           # saving the gradient of the current x_k
             g_k = np.array(self.opt_problem.gradient_value(x_k))
-           # updating the x_(k+1)
             x_k = x_k + delta_k
-           # saving the gradient of the updated x_k
             g_updated = np.array(self.opt_problem.gradient_value(x_k))
-           # calculating gamma
-            gamma_k =  g_updated - g_k;
-           # temporary variables for better reading of the code i guess?
-           # NxN matrix/scalar
-            dTg =  np.dot(np.transpose(delta_k), gamma_k)
-           
-           #  /NxN matrix
-            ddT = np.dot(delta_k, np.transpose(delta_k))     
-            HggTH = H_k @ gamma_k  @ np.transpose(gamma_k) * H_k
-            gTHg = np.transpose(gamma_k) @ H_k @ gamma_k  
-            H_k = H_k  + 0.0001 * np.eye(len(x_k), dtype= float) + ddT/ dTg  - HggTH / gTHg
-            i= i+1
+            gamma_k =  g_updated - g_k
+            
+            dTg =  np.dot(np.transpose(delta_k), gamma_k)          
+            ddT = np.dot(delta_k, np.transpose(delta_k))             
+            HggTH = np.dot(np.dot(H_k, gamma_k),np.transpose(gamma_k)) *H_k
+            gTHg = np.dot(np.dot(np.transpose(gamma_k), H_k ), gamma_k)
+            
+            H_k = H_k  + reg + ddT/ dTg  - HggTH / gTHg
             s_k = np.dot(-H_k,np.array(self.opt_problem.gradient_value(x_k)))
+            i= i+1
         print("Number of iterations DFP:", i-1)
         return x_k
         
--- project2/optimization/test_wolfe_search.py
+++ project2/optimization/test_wolfe_search.py
@@ -12,36 +12,59 @@ from optimization_problem import OptimizationProblem
 from methods import OptimizationMethod
 import chebyquad_problem as cp
 import dumitras_quasi_methods as quasi
+import matplotlib.pyplot as plt
 
 def obj_func(x):
     return (x[0])**2+(x[1])**2
 def obj_grad(x):
     return [2*x[0], 2*x[1]]
+def task12(inverse_hessians, true_inv_hessian):
+    """
+    Calculates the mean squared error of the inverse hessian approximations
+    as compared to the "true" inverse hessian and plots it against the 
+    number of iterations
+    
+    hessians : list of the hessians as returned by the minimize_bfgs()
+    true_hessian : the hessian computed by inverting the approximation 
+    calculated in Newton class. Can be done with np.linalg.inv()
+    
+    """
+    mse = []
+    for i in range(len(inverse_hessians)):
+        mse.append( np.mean((inverse_hessians[i] - (true_inv_hessian))**2))
+    print(mse)
+    plt.plot(mse)
+    plt.ylabel('mse')
+    plt.xlabel('k')
+    plt.show()
+    
+    
+
 
 def main():
 
-    xk = np.array([1.34, 1.45,1,1.5,1,1,1,1.5])
-    xk=np.linspace(0,1,11) #This is our initial guess. Should we add that as an input to our methods as well?
-    sk = np.array([-1.00, -1.00])
-   # alfa = line_search(cp.chebyquad, cp.gradchebyquad, xk, sk)[0]
-   # print("scipy search",alfa)
-    #print("my search",alfa)
-    
+    # i think this should not be user defined, it should be a paramter that cannot be chnaged easily
+    xk=np.linspace(0,1,11)    
     problem = OptimizationProblem(cp.chebyquad,cp.gradchebyquad)
-  #  problem = OptimizationProblem(obj_func, obj_grad)
-    #line_search_test = OptimizationMethod(problem)
-    #alfa = line_search_test.line_search_wolfe(xk, sk)
-    
-    test_bfgs = quasi.QuasiNewtonMethod(problem)
-   
-    x_k = test_bfgs.minimize_bfgs(xk, 1e-2,100)
+
+    test_bfgs = quasi.QuasiNewtonMethod(problem)  
+    x_k, hessians = test_bfgs.minimize_bfgs(xk, 1e-2,100, save_H= True)
     print("BFGS", x_k)
+   # print(hessians)
+    task12(hessians, np.linalg.inv(test_bfgs.hessian_aprox(xk, 1e-6)))
+    # true_hessian = test_bfgs.hessian_aprox(xk, 1e-6)
+    # mse = ((hessians - true_hessian)**2).mean()
+    # print("MSE_shpe", len(hessians))
+    #print(true_hessian)
+
    
-    x_dfp = test_bfgs.minimize_DFP(xk)
+    x_dfp = test_bfgs.minimize_DFP(xk, maxIters = 100)
     print("DFP", x_dfp)
     x_so = so.fmin_bfgs(cp.chebyquad, xk)
     print("BFGS scipy", x_so)
     print("cheby value for xk",cp.chebyquad(x_dfp))
+    
+    
 if __name__ == "__main__":
 
 
