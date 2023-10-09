6e34b6a93b93b967ef589c3c66d14a3b2677b2af
Tinnis97 <lina.tinnerberg@gmail.com>
2023-10-09 19:08:38 +0200

    Update room.py

--- project3/room.py
+++ project3/room.py
@@ -51,17 +51,19 @@ class Room():
     
     def update_dirichlt_condition(self,wall,start,new_value):
         s = int(start/self.dx)
+        dirichlt_value = np.array([])
         for i in range(len(new_value)):
             if(wall=='right'):
                 index = (i+s+1)*self.x_size-1
-                self.b[index][0] = self.b[index][0]+self.right[i+s]/(self.dx**2)
-                self.right[i+s] = new_value[i]
+                self.b[index][0] = self.b[index][0]-self.right[i+s]
+                self.right[i+s] = -new_value[i]/(self.dx**2)
             elif(wall=='left'):
                 index = (i+s)*self.x_size
-                self.b[index][0] = self.b[index][0]+self.left[i+s]/(self.dx**2)
-                self.left[i+s] = new_value[i]
-        self.add_dirichlt_condition(new_value, wall, start)
-        return None
+                self.b[index][0] = self.b[index][0]-self.left[i+s]
+                self.left[i+s] = -new_value[i]/(self.dx**2)
+            dirichlt_value = np.append(dirichlt_value,-new_value[i]/(self.dx**2))
+        self.add_dirichlt_condition(dirichlt_value, wall, start)
+    
     
     #wall can be a string that is top, bottom,left,or right
     def add_dirichlt_condition(self,value,wall,start):
@@ -79,45 +81,58 @@ class Room():
             elif(wall=='right'):
                 index = (i+start+1)*self.x_size-1
                 assert(index<self.v_size)
-            self.b[index][0] = self.b[index][0]-value[i]/(self.dx**2)
+            self.b[index][0] = self.b[index][0]+value[i]
     
-    def update_neuman_condition(self,wall,start,new_value):
-        
-        return None
+    
+    def update_neuman_condition(self,wall,new_value):
+        for i in range(len(new_value)):
+            if(wall=='right'):
+                index = (i+1)*self.x_size-1
+                self.b[index][0] = self.b[index][0]-self.right[i]
+            elif(wall=='left'):
+                index = (i)*self.x_size
+                self.b[index][0] = self.b[index][0]-self.left[i]
+        self.add_neuman_condition(new_value,wall)
+
             
-    def add_neuman_condition(self,u,value,wall):
+    def add_neuman_condition(self,value,wall):
+        wall_update = np.array([])
         for i in range(len(value)):
             if(wall=='right'):
                 index = (i+1)*self.x_size-1
             elif(wall=='left'):
                 index = i*self.x_size
-            
-            self.b[index][0] = self.b[index][0]-(value[i]-u[i])/(self.dx**2)
-            self.a[index][index] = self.a[index][index]/(self.dx**2)
+            neuman_value = -(value[i]-self.u_current[index])/(self.dx**2)
+            wall_update = np.append(wall_update,neuman_value)
+            self.b[index][0] = self.b[index][0]+neuman_value
         
+        if(wall=='right'):
+            self.right = wall_update
+        elif(wall=='left'):
+            self.left = wall_update
         return None
 
     
     #a neumann wall has to be a full wall
-    def add_neumann_wall(self,wall):
+    def add_neumann_wall(self,wall,value):
         if(wall=='right'):
             new_x_size = self.x_size +1
             for i in range(len(self.right)):
                 index = (i+1)*self.x_size-1
-                self.b[index][0] = self.b[index][0]+self.right[i]/(self.dx**2)
+                self.b[index][0] = self.b[index][0]-self.right[i]
             b_reshape = np.reshape(self.b,(self.y_size,self.x_size))
             self.b_add_column = np.c_[b_reshape, np.zeros((self.x_size,1))]
-            self.b_add_column[0][new_x_size-1] = -self.bottom[self.x_size-1]/(self.dx**2)
-            self.b_add_column[self.y_size-1][new_x_size-1] = -self.top[self.x_size-1]/(self.dx**2)
+            self.b_add_column[0][new_x_size-1] = +self.bottom[self.x_size-1]
+            self.b_add_column[self.y_size-1][new_x_size-1] = +self.top[self.x_size-1]
             self.bottom = np.append(self.bottom,self.bottom[self.x_size-1])
             self.top = np.append(self.top,self.top[self.x_size-1])
             #update the u_new to contain the previous boundary values
-            self.u_new = np.reshape(self.u_new,(self.y_size,self.x_size))
-            self.u_new = np.c_[self.u_new,self.right]
+            self.u_current = np.reshape(self.u_current,(self.y_size,self.x_size))
+            self.u_current = np.c_[self.u_current,-self.right*self.dx**2]
             self.x_size = new_x_size
             #I'll just recreate the A matrix,seems easier. 
             self.v_size = int(self.x_size*self.y_size)
-            self.u_new = np.reshape(self.u_new,((self.v_size,1)))
+            self.u_current = np.reshape(self.u_current,((self.v_size,1)))
             a_r = np.zeros(self.v_size)
             a_c = np.zeros(self.v_size)
             a_r[0] = -4
@@ -132,25 +147,26 @@ class Room():
             for i in range(self.y_size):
                 a[(1+i)*(self.x_size)-1][(1+i)*(self.x_size)-1]= -3
             self. a = 1/(self.dx**2)*a
-            self.b = np.reshape(self.b_add_column,(self.x_size*self.y_size))
+            self.b = np.reshape(self.b_add_column,(self.x_size*self.y_size,1))
+            
             
         elif(wall=='left'):
             new_x_size = self.x_size +1
             for i in range(len(self.left)):
                 index = (i)*self.x_size
-                self.b[index][0] = self.b[index][0]+self.left[i]/(self.dx**2)
+                self.b[index][0] = self.b[index][0]-self.left[i]
             b_reshape = np.reshape(self.b,(self.y_size,self.x_size))
             self.b_add_column = np.c_[np.zeros((self.x_size,1)),b_reshape]
-            self.b_add_column[0][0] = -self.bottom[self.x_size-1]/(self.dx**2)
-            self.b_add_column[self.y_size-1][0] = -self.top[self.x_size-1]/(self.dx**2)
+            self.b_add_column[0][0] = +self.bottom[self.x_size-1]
+            self.b_add_column[self.y_size-1][0] = +self.top[self.x_size-1]
             self.bottom = np.append(self.bottom,self.bottom[self.x_size-1])
             self.top = np.append(self.top,self.top[self.x_size-1])
             #update the u_new to contain the previous boundary values
-            self.u_new = np.reshape(self.u_new,(self.y_size,self.x_size))
-            self.u_new = np.c_[self.left,self.u_new]
+            self.u_current = np.reshape(self.u_current,(self.y_size,self.x_size))
+            self.u_current = np.c_[-self.left*self.dx**2,self.u_current]
             self.x_size = new_x_size
             self.v_size = int(self.x_size*self.y_size)
-            self.u_new = np.reshape(self.u_new,((self.v_size,1)))
+            self.u_current = np.reshape(self.u_current,((self.v_size,1)))
             a_r = np.zeros(self.v_size)
             a_c = np.zeros(self.v_size)
             a_r[0] = -4
@@ -165,17 +181,18 @@ class Room():
             for i in range(self.y_size):
                 a[(i)*(self.x_size)][(i)*(self.x_size)] = -3
             self. a = 1/(self.dx**2)*a
-            self.b = np.reshape(self.b_add_column,(self.x_size*self.y_size))
+            self.b = np.reshape(self.b_add_column,(self.x_size*self.y_size,1))
+        self.add_neuman_condition(value,wall)
 
             
     
     #creates our initial walls
     def create_walls(self, wall_bottom, wall_top, wall_right, wall_left):
         
-        self.bottom = np.full((self.x_size),wall_bottom)
-        self.top = np.full((self.x_size),wall_top)
-        self.right = np.full((self.y_size),wall_right)
-        self.left = np.full((self.y_size),wall_left)
+        self.bottom = -np.full((self.x_size),wall_bottom)/(self.dx**2)
+        self.top = -np.full((self.x_size),wall_top)/(self.dx**2)
+        self.right = -np.full((self.y_size),wall_right)/(self.dx**2)
+        self.left = -np.full((self.y_size),wall_left)/(self.dx**2)
         self.add_dirichlt_condition(self.bottom, 'bottom', 0)
         self.add_dirichlt_condition(self.top, 'top', 0)
         self.add_dirichlt_condition(self.right, 'right', 0)
@@ -208,16 +225,21 @@ class Room():
             u_bound = np.append(u_bound,u[index])
         
         return u_bound
+    
+    def relax(self,omega):
+        #for i in range(len(u_new)):
+        self.u_new= omega*self.u_new+(1-omega)*self.u_current
+        self.update()
 
 #def main():
 
 #initialization
-room_two = Room(np.array([1,2]),1/3)
+room_two = Room(np.array([1,2]),1/20)
 room_two.create_walls(5,40,15,15)
 u_two = room_two.solve()
 
-room_one = Room(np.array([1,1]),1/3)
-room_three = Room(np.array([1,1]),1/3)
+room_one = Room(np.array([1,1]),1/20)
+room_three = Room(np.array([1,1]),1/20)
 room_one.create_walls(15,15,15,40)
 u_one = room_one.solve()
 
@@ -230,22 +252,34 @@ u_one = np.c_[u_one,np.full((room_one.x_size,1),15)]
 u_three = np.reshape(u_three,(room_three.y_size,room_three.x_size))
 u_three = np.c_[np.full((room_three.x_size,1),15),u_three]
 
-room_one.add_neumann_wall('right')
-room_three.add_neumann_wall('left')
 room_one.update()
 room_two.update()
 room_three.update()
+
+u_two_new_left = room_two.get_boundary_values('old', 1, 0, 'left')
+u_two_new_right = room_two.get_boundary_values('old', 1, 1, 'right')
+room_one.add_neumann_wall('right',u_two_new_left)
+room_three.add_neumann_wall('left',u_two_new_right)
+
 #start the iteration
-
-u_one_old = room_one.get_boundary_values('old',1,0,'right')
-u_three_old = room_three.get_boundary_values('old',1,0,'left')
-room_two.update_dirichlt_condition('right', 1, u_three_old)
-room_two.update_dirichlt_condition('left', 0, u_one_old)
-room_two.solve()
-
-u_two_new_left = room_two.get_boundary_values('new', 1, 0, 'left')
-u_two_new_right = room_two.get_boundary_values('new', 1, 1, 'right')
-#room_one.
+for i in range(2):
+    u_one_old = room_one.get_boundary_values('old',1,0,'right')
+    u_three_old = room_three.get_boundary_values('old',1,0,'left')
+    room_two.update_dirichlt_condition('right', 1, u_three_old)
+    room_two.update_dirichlt_condition('left', 0, u_one_old)
+    room_two.solve()
+    
+    u_two_new_left = room_two.get_boundary_values('new', 1, 0, 'left')
+    u_two_new_right = room_two.get_boundary_values('new', 1, 1, 'right')
+    room_one.update_neuman_condition('right', u_two_new_left)
+    room_three.update_neuman_condition('left', u_two_new_right)
+    room_one.solve()
+    room_three.solve()
+    
+    #do the realaxation. 
+    room_one.relax(0.8)
+    room_two.relax(0.8)
+    room_three.relax(0.8)
 
 #if __name__ == "__main__":
 #    main()
