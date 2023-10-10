c077526b6bd96c5f7cd80c0525f50b4263de5187
Filippa Hansen <filippahansen@Filippas-MacBook-Pro.local>
2023-10-10 16:03:15 +0200

    updated ready for test

--- project3/main.py
+++ project3/main.py
@@ -16,19 +16,23 @@ dx = 1/3
 room_two = Room(np.array([1,2]),dx) # size 1x2
 room_two.create_walls(5,40,15,15)
 u_two = room_two.solve()
+room_two.update()
+u_two_new_left = room_two.get_boundary_values('old', 1, 0, 'left')
+u_two_new_right = room_two.get_boundary_values('old', 1, 1, 'right')
 
 ## ROOM 1
 room_one = Room(np.array([1,1]),dx) # size 1x1
 room_one.create_walls(15,15,15,40)
 u_one = room_one.solve()
-u1_km1 = u_one
+room_one.update()
+room_one.add_neumann_wall('right',u_two_new_left)
 
 ## ROOM 3
 room_three = Room(np.array([1,1]),dx) # size 1x1
 room_three.create_walls(15,15,40,15)
 u_three = room_three.solve()
-u3_km1 = u_three
-
+room_three.update()
+room_three.add_neumann_wall('left',u_two_new_right)
 
 # rank 0 is room 2
 # rank 1 is room 1
@@ -43,19 +47,26 @@ for iter in range(iterations_count):
     if rank == 0: # TODO maybe modify rank nbr after order and not by room order
         print("Rank is 0")
         if(iter==0): #first iteration
-            ## TODO intitial solver from Room for room 2
-            break
+            bounds_r1 = room_two.get_boundary_values('new', 1, 0, 'left')
+            bounds_r3 = room_two.get_boundary_values('new', 1, 1, 'right')
+            comm.send(bounds_r1, dest = 1)
+            comm.send(bounds_r3, dest = 2)
+            room_two.relax(omega)
         else:
-            b1 = comm.recv(source = 1) # boundary for room 1
-            b3 = comm.recv(source = 2) # boundary for room 3
-            ## TODO update boundaries for room1 and 3 with b1 and b3
-            ## TODO solve room 2 with new boundaries
+            u_one_old = comm.recv(source = 1) # boundary for room 1
+            u_three_old = comm.recv(source = 2) # boundary for room 3
+            ## update boundaries for room1 and 3 with b1 and b3
+            room_two.update_dirichlt_condition('right', 1, u_three_old)
+            room_two.update_dirichlt_condition('left', 0, u_one_old)
+            ## solve room 2 with new boundaries
+            room_two.solve()
 
-        ## TODO calculated_bound1 = ... calc new boundary for room 1 from room 2
-        ## TODO calculated_bound3 = ... calc new boundary for room 3 from room 2
+        bounds_r1 = room_two.get_boundary_values('new', 1, 0, 'left')
+        bounds_r3 = room_two.get_boundary_values('new', 1, 1, 'right')
 
-        ## TODO comm.send(calculated_bound1, dest = 1)
-        ## TODO comm.send(calculated_bound3, dest = 2)
+        comm.send(bounds_r1, dest = 1)
+        comm.send(bounds_r3, dest = 2)
+        room_two.relax(omega)
 
     # Step 2: On Room 1, receive v_k_r from Room 2 and solve left system
     if rank == 1:
@@ -63,9 +74,11 @@ for iter in range(iterations_count):
         bounds_r1 = comm.recv(source = 0)
        
        ## TODO update boundaries for room 1
-        u1_kp1 = room_one.solve()
-        u1_kp1 = omega*u1_kp1 +(1-omega)*u1_km1
-        u1_km1 = u1_kp1
+        room_one.update_neuman_condition('right', bounds_r1)
+        room_one.solve()
+        room_one.relax(omega)
+        u1_kp1 = room_one.get_boundary_values('old',1,0,'right')
+
         comm.send(u1_kp1, dest = 0) ## send to room 2
 
     if rank == 2:
@@ -73,24 +86,26 @@ for iter in range(iterations_count):
         bounds_r3 = comm.recv(source = 0)
        
         ## TODO update boundaries for room 3
-        u3_kp1 = room_three.solve()
-        u3_kp1 = omega*u3_kp1 + (1-omega)*u3_km1
-        u3_km1 = u3_kp1
+        room_three.update_neuman_condition('left', bounds_r3)
+        room_three.solve()
+        room_three.relax(omega)
+        u3_kp1 = room_three.get_boundary_values('old',1,0,'left')
+
         comm.send(u3_kp1, dest = 0)
 
-    if(iter == iterations_count-1):
-        if rank == 0:
-            comm.send(u_two, dest=3, tag=2)
-        if rank == 1:
-            comm.send(u_one, dest=3, tag=1)
-        if rank == 2:
-            comm.send(u_three, dest=3, tag=3)
+    # if(iter == iterations_count-1):
+    #     if rank == 0:
+    #         comm.send(u_two, dest=3, tag=2)
+    #     if rank == 1:
+    #         comm.send(u_one, dest=3, tag=1)
+    #     if rank == 2:
+    #         comm.send(u_three, dest=3, tag=3)
 
-    ## TODO add if rank == 3 --> plot ...
-    if rank == 3:
-        u_one = comm.recv(source = 1, tag=1)
-        u_two = comm.recv(source=0, tag=2)
-        u_three = comm.recv(source = 2, tag=3)
+    # ## TODO add if rank == 3 --> plot ...
+    # if rank == 3:
+    #     u_one = comm.recv(source = 1, tag=1)
+    #     u_two = comm.recv(source=0, tag=2)
+    #     u_three = comm.recv(source = 2, tag=3)
 
 
     
