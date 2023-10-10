db5fdbf5bbd8472d55e142684990f737cb7203d0
Filippa Hansen <filippahansen@Filippas-MacBook-Pro.local>
2023-10-10 16:51:10 +0200

    added 03a

--- project3/main2.py
+++ project3/main2.py
@@ -20,6 +20,7 @@ u_two = room_two.solve()
 room_two.update()
 u_two_new_left = room_two.get_boundary_values('old', 1, 0, 'left')
 u_two_new_right = room_two.get_boundary_values('old', 1, 1, 'right')
+u_two_new_right2 = room_two.get_boundary_values('old', 0.5, 0.5, 'right')
 
 ## ROOM 1
 room_one = Room(np.array([1,1]),dx) # size 1x1
@@ -35,9 +36,19 @@ u_three = room_three.solve()
 room_three.update()
 room_three.add_neumann_wall('left',u_two_new_right)
 
+## ROOM 4
+room_four = Room(np.array([0.5,0.5]),dx) 
+room_four.create_walls(40,15,15,15) # bottom, top, right, left
+u_four = room_four.solve()
+room_four.update()
+room_four.add_neumann_wall('left',u_two_new_right2)
+
+
+
 # rank 0 is room 2
 # rank 1 is room 1
 # rank 2 is room 3
+# rank 3 is room 4
 
 omega = 0.8
 iterations_count = 10
@@ -45,28 +56,35 @@ iterations_count = 10
 # Main iterative loop
 for iter in range(iterations_count):
     print(iter)
-    # Step 1: On Room 2, obtain v_k_r for room 1 and 3 and send new  bound to Room 1 and Room 3
+    # Step 1: On Room 2, obtain v_k_r for room 1, 4 and 3 and send new  bound to Room 1 and Room 3 and Room 4
     if rank == 0: # TODO maybe modify rank nbr after order and not by room order
         print("Rank is 0")
         if(iter==0): #first iteration
             bounds_r1 = room_two.get_boundary_values('old', 1, 0, 'left')
             bounds_r3 = room_two.get_boundary_values('old', 1, 1, 'right')
+            bounds_r4 = room_two.get_boundary_values('old', 0.5, 0.5, 'right')
+
             comm.send(bounds_r1, dest = 1)
             comm.send(bounds_r3, dest = 2)
+            comm.send(bounds_r4, dest = 3)
         else:
             u_one_old = comm.recv(source = 1) # boundary for room 1
             u_three_old = comm.recv(source = 2) # boundary for room 3
-            ## update boundaries for room1 and 3 with b1 and b3
+            u_four_old = comm.recv(source = 3) # boundary for room 4
+            ## update boundaries for room1, 4 and 3 with b1, b4 and b3
             room_two.update_dirichlt_condition('right', 1, u_three_old)
             room_two.update_dirichlt_condition('left', 0, u_one_old)
+            room_two.update_dirichlt_condition('right', 0.5, u_four_old)
             ## solve room 2 with new boundaries
             room_two.solve()
 
             bounds_r1 = room_two.get_boundary_values('new', 1, 0, 'left')
             bounds_r3 = room_two.get_boundary_values('new', 1, 1, 'right')
+            bounds_r4 = room_two.get_boundary_values('new', 0.5, 0.5, 'right')
 
             comm.send(bounds_r1, dest = 1)
             comm.send(bounds_r3, dest = 2)
+            comm.send(bounds_r4, dest = 3)
             room_two.relax(omega)
 
     # Step 2: On Room 1, receive v_k_r from Room 2 and solve left system
@@ -86,7 +104,7 @@ for iter in range(iterations_count):
         print("Rank is 2")
         bounds_r3 = comm.recv(source = 0)
        
-        ## TODO update boundaries for room 3
+        ##  update boundaries for room 3
         room_three.update_neuman_condition('left', bounds_r3)
         room_three.solve()
         room_three.relax(omega)
@@ -94,7 +112,18 @@ for iter in range(iterations_count):
 
         comm.send(u3_kp1, dest = 0)
 
-     
+    if rank == 3:
+        print("Rank is 3")
+        bounds_r4 = comm.recv(source = 0)
+       
+        ##  update boundaries for room 3
+        room_four.update_neuman_condition('left', bounds_r4)
+        room_four.solve()
+        room_four.relax(omega)
+        u4_kp1 = room_four.get_boundary_values('old',0.5,0.5,'right')
+
+        comm.send(u4_kp1, dest = 0)
+
 
     if(iter == iterations_count-1):
         if rank == 0:
@@ -113,6 +142,12 @@ for iter in range(iterations_count):
             u_3 = np.flipud(np.reshape(room_three.u_current,(3,4)))
             print('matrix three', u_3)
             plt.imshow(u_3, cmap='hot', interpolation='nearest')
+            plt.show()
+            # comm.send(u_three, dest=3, tag=3)
+        if rank == 3:
+            u_4 = np.flipud(np.reshape(room_four.u_current,(3,4)))
+            print('matrix three', u_4)
+            plt.imshow(u_4, cmap='hot', interpolation='nearest')
             plt.show()
             # comm.send(u_three, dest=3, tag=3)
 
